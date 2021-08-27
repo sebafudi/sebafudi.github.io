1a9765a41ccc4275e73114aafbaac6f43872c0c4
sebafudi <sebastianfudalej@gmail.com>
2021-08-27 12:20:08 +0200

    feat: show rendered markdown from README.md

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import base64 from "base-64";
+import marked from "marked";
 
 class Details extends Component {
   constructor() {
@@ -17,11 +19,32 @@ class Details extends Component {
       }
     );
     const json = await res.json();
-    this.setState(Object.assign({ loading: false }, json));
+    this.setState(Object.assign({ loading: false, status: res.status }, json));
   }
   render() {
-    const { content, encoding, name } = this.state;
-    return <h1>{name}</h1>;
+    const { status, content, loading } = this.state;
+    if (loading === false) {
+      if (status !== 200) {
+        return (
+          <section className="section">
+            <div className="notification is-warning">No README.md found</div>
+          </section>
+        );
+      }
+      let readme = base64.decode(content);
+      return (
+        <section className="section">
+          <div className="content">
+            <div dangerouslySetInnerHTML={{ __html: marked(readme) }} />
+          </div>
+        </section>
+      );
+    }
+    return (
+      <section className="section">
+        <div className="notification is-info">Loading</div>
+      </section>
+    );
   }
 }
 
