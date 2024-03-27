f48cfd4444835cf79931892987f981d46a91b667
Sebastian Fudalej <sebastianfudalej@gmail.com>
2024-03-27 23:51:34 +0100

    feat: add scroll animation

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,33 @@
 import './style.css';
+document.addEventListener("wheel", (e) => {
+    const delta = e.deltaY;
+    if (delta > 0) {
+        const text = document.querySelector('.text-active');
+        if (!text) return;
+        text.classList.remove('text-active');
+        setTimeout(() => {
 
-// const fadeFromBottom =
-//   document.querySelector<HTMLDivElement>('.fade-from-bottom');
-// if (fadeFromBottom) {
-//   fadeFromBottom.classList.add('settling');
-//   setTimeout(() => {
-//     fadeFromBottom.classList.remove('settling');
-//   }, 800);
-// }
+            const nextText = text.nextElementSibling;
+            if (nextText) {
+                nextText.classList.add('text-active');
+            } else {
+                text.classList.remove('text-active');
+                document.querySelector('.text').classList.add('text-active');
+            }
+        }, 300);
+    } else {
+        const text = document.querySelector('.text-active');
+        if (!text) return;
+        text.classList.remove('text-active');
+        setTimeout(() => {
+            const prevText = text.previousElementSibling;
+            if (prevText) {
+                prevText.classList.add('text-active');
+            } else {
+                text.classList.remove('text-active');
+                document.querySelector('.text:last-child').classList.add('text-active');
+            }
+        }, 300);
+    }
+});
 
-// const welcomeTexts = ['Hello!', 'Hej!', '¡Hola!'];
-// let welcomeTextIdx = 1;
-
-// const welcomeText = document.querySelector<HTMLDivElement>('.welcome-text');
-// setInterval(() => {
-//   if (welcomeText) {
-//     welcomeText.classList.add('settling');
-//     setTimeout(() => {
-//       welcomeText.innerHTML = welcomeTexts[welcomeTextIdx];
-//       welcomeTextIdx = (welcomeTextIdx + 1) % welcomeTexts.length;
-//       welcomeText.classList.remove('settling');
-//     }, 800);
-//   }
-// }, 5000);
