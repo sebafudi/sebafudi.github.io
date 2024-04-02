8b11afcca80aac0e4f0bfd783860af95257dea67
Sebastian Fudalej <sebastianfudalej@gmail.com>
2024-04-03 01:48:48 +0200

    feat: update project section

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,5 @@
 import './style.css';
-
-let scrollLock = false;
-document.addEventListener("wheel", (e) => {
-    if (scrollLock) return;
-    scrollLock = true;
-    const delta = e.deltaY;
-    if (delta > 0) {
-        const text = document.querySelector('.text-active');
-        if (!text) return;
-        text.classList.remove('text-active');
-        setTimeout(() => {
-
-            const nextText = text.nextElementSibling;
-            if (nextText) {
-                nextText.classList.add('text-active');
-            } else {
-                text.classList.remove('text-active');
-                document.querySelector('.text')?.classList.add('text-active');
-            }
-            setTimeout(() => {
-                scrollLock = false;
-            }, 300);
-        }, 300);
-    } else {
-        const text = document.querySelector('.text-active');
-        if (!text) return;
-        text.classList.remove('text-active');
-        setTimeout(() => {
-            const prevText = text.previousElementSibling;
-            if (prevText) {
-                prevText.classList.add('text-active');
-            } else {
-                text.classList.remove('text-active');
-                document.querySelector('.text:last-child')?.classList.add('text-active');
-            }
-            setTimeout(() => {
-                scrollLock = false;
-            }, 300);
-        }, 300);
-    }
-});
-
-const texts = document.querySelectorAll('.text');
+const texts = document.querySelectorAll('.fading-text');
 
 texts.forEach((text) => {
     text.addEventListener('mouseover', () => {
